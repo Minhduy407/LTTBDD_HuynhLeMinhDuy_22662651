dbb38d73d007c3dacc962ace83303f13e4b10afc
MinhDuy <minhdii1510@gmail.com>
2025-09-02 10:17:47 +0700

    push Tuan2

---
+++ Tuan2/dist/async.js
@@ -0,0 +1,43 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.resolveWithTen = resolveWithTen;
+function resolveWithTen() {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(10), 1000);
+    });
+}
+async function getTenAsync() {
+    const result = await resolveWithTen();
+    console.log("Async result: ", result);
+}
+getTenAsync();
+function simulateTask(time) {
+    return new Promise(resolve => {
+        setTimeout(() => resolve("Task done in " + time + "ms"), time);
+    });
+}
+async function runSimulateTask() {
+    const result = await simulateTask(2000);
+    console.log("Task done");
+}
+runSimulateTask();
+function rejectWithError() {
+    return new Promise((_, reject) => {
+        setTimeout(() => reject(new Error("Something went wrong")), 1000);
+    });
+}
+async function tryCatchExample() {
+    try {
+        const result = await rejectWithError();
+        console.log("Should not log: ", result);
+    }
+    catch (err) {
+        if (err instanceof Error) {
+            console.log("Caught error:", err.message);
+        }
+        else {
+            console.log("Unknown error:", err);
+        }
+    }
+}
+tryCatchExample();
---
+++ Tuan2/dist/promise.js
@@ -0,0 +1,73 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.resolveWithTen = resolveWithTen;
+const helloAsync = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Hello Async");
+    }, 2000);
+});
+helloAsync.then((message) => {
+    console.log(message);
+});
+function resolveWithTen() {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(10), 1000);
+    });
+}
+resolveWithTen().then(num => console.log("Resolve num: ", num));
+function rejectWithError() {
+    return new Promise((_, reject) => {
+        setTimeout(() => reject(new Error("Something went wrong")), 1000);
+    });
+}
+rejectWithError().then(() => { }).catch(err => console.log("Rejected with: ", err.message));
+function getRamdomNumber() {
+    return new Promise((resolve, reject) => {
+        const num = Math.random();
+        if (num >= 0)
+            resolve(num);
+        else
+            reject("Invalid number");
+    });
+}
+getRamdomNumber()
+    .then(num => console.log("Random number: ", num))
+    .catch(err => console.error("Error: ", err));
+function simulateTask(time) {
+    return new Promise(resolve => {
+        setTimeout(() => resolve("Task done in " + time + "ms"), time);
+    });
+}
+simulateTask(1000).then(res => console.log("Simulate task: ", res));
+Promise.all([
+    simulateTask(1000),
+    simulateTask(2000),
+    simulateTask(1500)
+]).then(results => {
+    console.log("All tasks finished: ", results);
+});
+Promise.race([
+    simulateTask(1000),
+    simulateTask(2000),
+    simulateTask(1500)
+]).then(result => {
+    console.log("First task finished: ", result);
+});
+Promise.resolve(2)
+    .then(num => num * num)
+    .then(num => num * 2)
+    .then(num => num + 5)
+    .then(result => console.log("Final result: ", result));
+function filterEvenNumbers(arr) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(arr.filter(num => num % 2 === 0));
+        }, 1000);
+    });
+}
+filterEvenNumbers([1, 2, 3, 4, 5, 6])
+    .then(result => console.log("Even numbers: ", result));
+getRamdomNumber()
+    .then(num => console.log("Number: ", num))
+    .catch(err => console.error("Error: ", err))
+    .finally(() => console.log("Done"));
